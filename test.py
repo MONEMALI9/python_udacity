f77f11eebcdf4708c1d33591d16c02216d6e2c15
MONEMALI9 <monem921999@gmail.com>
2023-08-27 14:31:06 +0300

    .

---
+++ test.py
@@ -0,0 +1,21 @@
+# why use python?
+    #ease - to -use
+    #we can use it widly in different fields
+    
+# python version type
+    #python 2
+    #python 3
+    
+# how to install and run python?
+ #python.org
+
+# installing ipython 
+    #pip install ipython >> windows
+    #pip3 install ipython >> linux
+    
+# ipython  VS python shell
+ # magic function
+  #pip >> Python Install Package
+# packages >> advanced topic >> numpy , pandas
+# how to use jupyter notebook
+print("hello")
